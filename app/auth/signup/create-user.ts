e7ed34e578d415a7941a9b4b19bf7b6572edd915
Maria <mlatysheva@gmail.com>
2025-02-14 21:35:00 +0100

    feat: added createUser server action

---
+++ app/auth/signup/create-user.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { redirect } from 'next/navigation';
+import { API_URL } from '../../constants/api';
+
+export default async function createUser( _prevState: any, formData: FormData) {
+  const response = await fetch(`${API_URL}/users`, {
+    method: 'POST',
+    body: formData,
+  });
+
+  const parsedResponse = await response.json();
+
+  if (!response.ok) {
+    console.log(parsedResponse);
+    return {error: ""}
+  }
+
+  redirect('/');
+}
