9c22279d2393de7b32fdbf2cd40cf169bf203c49
Maria <mlatysheva@gmail.com>
2025-03-03 23:08:41 +0100

    feat: added header and the routes array

---
+++ app/shared/constants/routes.ts
@@ -0,0 +1,17 @@
+export const publicRoutes = [
+  {
+    title: "Login",
+    path: "/auth/login",
+  }, 
+  {
+    title: "Signup",
+    path: "/auth/signup",
+  }
+]
+
+export const routes = [
+  {
+    title: "Home",
+    path: "/",
+  }
+]
