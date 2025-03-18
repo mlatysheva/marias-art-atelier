43c932c7011c47f8ae098bb63f86189c055182b6
Maria <mlatysheva@gmail.com>
2025-03-18 22:02:06 +0100

    fix: replaced the blue autofill input color with default

--- app/dark.theme.ts
+++ app/dark.theme.ts
@@ -12,6 +12,20 @@ const darkTheme = createTheme({
       main: '#47a6a6'
     }
   },
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "& input:-webkit-autofill": {
+            WebkitBoxShadow: "0 0 0 100px #121212 inset !important", // Autofill background
+            WebkitTextFillColor: "#fff !important",
+            caretColor: "#fff !important",
+            borderRadius: "inherit",
+          },
+        },
+      },
+    },
+  },
 });
 
 export default darkTheme;
