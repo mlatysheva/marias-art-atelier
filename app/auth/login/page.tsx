24960138fca7d80a0fbbdeb6b127d8740194a735
Maria <mlatysheva@gmail.com>
2025-03-18 22:32:10 +0100

    feat: updated signup form to include additional fields - updated error rendering in login and signup

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Link, Stack, TextField } from '@mui/material';
+import { Alert, Button, Link, Stack, TextField } from '@mui/material';
 import NextLink from 'next/link';
 import { useActionState } from 'react';
 import login from './login';
@@ -11,10 +11,17 @@ export default function Login() {
   return (
     <form action={formAction} className='w-full max-w-xs'>
       <Stack spacing={2} >
-        <TextField name="email" label='Email' variant='outlined' type='email' error={!!state.error} helperText={state.error} />
-        <TextField name="password" label='Password' variant='outlined' type='password' error={!!state.error} helperText={state.error} />
+        <TextField name="email" label='Email' variant='outlined' type='email' />
+        <TextField name="password" label='Password' variant='outlined' type='password' />
         <Button type="submit" variant='contained'>Login</Button>
         <Link component={NextLink} href='/auth/signup' className='self-center'>Signup</Link>
+        {state?.error && (
+          <Alert severity="error">
+            {state.error.split(',').map((msg: string, index: number) => (
+              <div key={index}>{msg}</div>
+            ))}
+          </Alert>
+        )}
       </Stack>
     </form>
     
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,19 +1,30 @@
 "use client";
 
-import { Button, Link, Stack, TextField } from '@mui/material';
+import { Alert, Button, Link, Stack, TextField } from '@mui/material';
 import NextLink from 'next/link';
 import createUser from './create-user';
 import { useActionState } from 'react';
 
 export default function Signup() {
   const [state, formAction] = useActionState(createUser, {error: ""});
+
   return (
     <form action={formAction} className='w-full max-w-xs'>
       <Stack spacing={2}>
-        <TextField name="email" label='Email' variant='outlined' type='email' helperText={state.error} error={!!state.error} />
-        <TextField name="password" label='Password' variant='outlined' type='password' helperText={state.error} error={!!state.error} />
+        <TextField name="firstName" label='First Name' variant='outlined' />
+        <TextField name="lastName" label='Last Name' variant='outlined' />
+        <TextField name="username" label='Username' variant='outlined' required />
+        <TextField name="email" label='Email' variant='outlined' type='email' required />
+        <TextField name="password" label='Password' variant='outlined' type='password' />
         <Button variant='contained' type="submit">Signup</Button>
         <Link component={NextLink} href='/auth/login' className='self-center'>Login</Link>
+        {state?.error && (
+          <Alert severity="error">
+            {state.error.split(',').map((msg: string, index: number) => (
+              <div key={index}>{msg}</div>
+            ))}
+          </Alert>
+        )}
       </Stack>
     </form>
   );
